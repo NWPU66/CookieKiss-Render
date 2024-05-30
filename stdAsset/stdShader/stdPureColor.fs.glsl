44baf00e3600ece6054c939ec2730f9e952c8b10
NWPU66 <18050023358@163.com>
2024-05-30 14:49:16 +0800

    创建项目并初始化git仓库

---
+++ stdAsset/stdShader/stdPureColor.fs.glsl
@@ -0,0 +1,31 @@
+#version 460 core
+//定义灯光组的最大灯光数量
+#define MAX_LIGHTS_NUM 16
+//input
+in VS_OUT{
+    vec3 globalPos;
+    vec3 globalNormal;
+    vec2 texCoord;
+}fs_in;
+
+struct Light{
+    int lightType;
+    vec3 color;
+    float intensity;
+    vec3 position,rotation;
+    float innerCutOff,outerCutOff;// for spot light
+};
+
+layout(std140,binding=0)uniform lightGroup{
+    int numLights;
+    Light lights[MAX_LIGHTS_NUM];
+};
+
+uniform vec3 lightColor;
+
+//output
+out vec4 fragColor;
+
+void main(){
+    fragColor=vec4(lightColor,1);
+}
