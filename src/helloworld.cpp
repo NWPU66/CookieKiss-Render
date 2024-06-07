5941c255c7561c0be4502728589c8d60daad4cc3
NWPU66 <53937577+NWPU66@users.noreply.github.com>
2024-06-07 19:13:44 +0800

    modified:   .gitignore 	new file:   CMakeLists.txt 	new file:   src/CMakeLists.txt 	new file:   src/IET/CMakeLists.txt 	new file:   src/core/CMakeLists.txt 	new file:   src/demo/CMakeLists.txt 	renamed:    demo/demo_HelloOpenGL/imgui_impl_glfw.cpp -> src/demo/demo_HelloOpenGL/imgui_impl_glfw.cpp 	renamed:    demo/demo_HelloOpenGL/imgui_impl_opengl3.cpp -> src/demo/demo_HelloOpenGL/imgui_impl_opengl3.cpp 	renamed:    demo/demo_HelloOpenGL/main.cpp -> src/demo/demo_HelloOpenGL/main.cpp 	renamed:    demo/demo_HelloOpenGL/triangle.fs.glsl -> src/demo/demo_HelloOpenGL/triangle.fs.glsl 	renamed:    demo/demo_HelloOpenGL/triangle.vs.glsl -> src/demo/demo_HelloOpenGL/triangle.vs.glsl 	renamed:    demo/demo_ImguiVulkanExample/imgui_impl_glfw.cpp -> src/demo/demo_ImguiVulkanExample/imgui_impl_glfw.cpp 	renamed:    demo/demo_ImguiVulkanExample/imgui_impl_vulkan.cpp -> src/demo/demo_ImguiVulkanExample/imgui_impl_vulkan.cpp 	renamed:    demo/demo_ImguiVulkanExample/main.cpp -> src/demo/demo_ImguiVulkanExample/main.cpp 	new file:   src/helloworld.cpp 	new file:   src/util/CMakeLists.txt 	new file:   src/util/stdInclude.h 	new file:   src/util/util.cpp

---
+++ src/helloworld.cpp
@@ -0,0 +1,14 @@
+#include "util/stdInclude.h"
+
+#include "glm/matrix.hpp"
+#include "imgui.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Welcome to CookieKiss Render!" << std::endl;
+    glm::mat4 m = glm::mat4(1.0f);
+    std::cout << glm::determinant(m) << std::endl;
+    IMGUI_CHECKVERSION();
+    return 0;
+}
---
+++ src/util/stdInclude.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/**NOTE -  #include 的路径及顺序
+ * dir2/foo2.h.
+ * C 语言系统文件 (确切地说: 用使用方括号和 .h 扩展名的头文件), 例如 <unistd.h> 和 <stdlib.h>.
+ * C++ 标准库头文件 (不含扩展名), 例如 <algorithm> 和 <cstddef>.
+ * 其他库的 .h 文件.
+ * 本项目的 .h 文件.
+ */
+
+// C Library
+#include <stdexcpt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// C++ Library
+#include <string>
+#include <vector>
+#include <iostream>
