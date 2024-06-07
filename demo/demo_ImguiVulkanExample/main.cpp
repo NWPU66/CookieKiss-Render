5cd7d8a9abf51ec1080e8d94a4fec1d718204915
NWPU66 <18050023358@163.com>
2024-06-07 09:30:05 +0800

    modified:   .gitmodules 	modified:   .vscode/c_cpp_properties.json 	modified:   .vscode/settings.json 	new file:   3rdparty/bullet3 	modified:   README.md 	modified:   demo/demo_ImguiVulkanExample/main.cpp
    	modified:   3rdparty/bullet3 (modified content, untracked content)

--- demo/demo_ImguiVulkanExample/main.cpp
+++ demo/demo_ImguiVulkanExample/main.cpp
@@ -2,7 +2,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_vulkan.h"
 #include "stdInclude.h"
-#include "spdlog/spdlog.h"
+
 #define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
